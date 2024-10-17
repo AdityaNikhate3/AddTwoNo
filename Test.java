d1e5da9df7f8de171a07025592c409120e9796ca
AdityaNikhate3 <adityanikhateacts@gmail.com>
2024-10-17 13:44:10 +0530

    Add new code

--- Test.java
+++ Test.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
 class Test{
 	public static void main(String[] args){
-		System.out.println("hi");
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter two no");
+		int a = sc.nextInt(), b = sc.nextInt();
+
+		System.out.println("Sum is: "+(a+b));
+		sc.close();
 	}
 }
